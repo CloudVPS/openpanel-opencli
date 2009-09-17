26e2ba57c5bb92938a1b4c7feaa83d68d873d5fd
Pim van Riezen <pi@openpanel.com>
2009-09-17 10:19:19 +0200

    debugging

--- main.cpp
+++ main.cpp
@@ -445,6 +445,7 @@ string *opencliApp::uuidToUser (const statstring &uuid)
 // ==========================================================================
 int opencliApp::cmdShow (const value &argv)
 {
+	fout.writeln (">> cmdShow");
 	if (ctx.atRoot())
 	{
 		fout.writeln ("% No local data at this level");
@@ -457,14 +458,20 @@ int opencliApp::cmdShow (const value &argv)
 					$("value", $attr("bold",false)->$val("Value")) ->
 					$("desc", $attr("bold",false)->$val("Description"));
 
+
+	fout.writeln (">> cmdShow ctx.currentClass()");
 	statstring classid = ctx.currentClass ();
+	fout.writeln (">> cmdShow new coreclass");
 	coreclass cclass (conn, classid);
 	
+	fout.writeln (">> cmdShow cclass.parameters()");
 	value params = cclass.parameters ();
 
+	fout.writeln (">> uuidtouser");
 	string uname = uuidToUser (crecord["ownerid"]);
 	if (! uname) uname = "root";
 
+	fout.writeln (">> cmdShow tail");
 	vdata.newval() =
 		$("field", "owner") ->
 		$("value", uname) ->
